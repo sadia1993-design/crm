081c25984d3fc7416e1e887ce89473b70f72014c
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2022-03-23 03:12:44 +0600

    1

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -1,49 +1,49 @@
 <?php
- 
+
 namespace App\Http\Controllers;
 use App\Models\Departments;
 use Illuminate\Http\Request;
- 
-class departmentController extends Controller
+
+class DepartmentController extends Controller
 {
-  
+
     public function index()
     {
         $contacts = Departments::all();
       return view ('admin.departments.index')->with('contacts', $contacts);
     }
- 
-    
+
+
     public function create()
     {
         return view('admin.departments.create');
     }
- 
-  
+
+
     public function store(Request $request)
     {
         $request->validate([
        'name' => 'required',]);
         $input = $request->all();
         Departments::create($input);
-        return redirect('admin/department')->with('success', 'Departments created successfully');  
+        return redirect('admin/department')->with('success', 'Departments created successfully');
     }
- 
-    
+
+
     public function show($id)
     {
-        
+
     }
- 
-    
+
+
     public function edit($id)
     {
-         
+
         $contact = Departments::find($id);
         return view('admin.departments.edit')->with('contacts', $contact);
     }
- 
-  
+
+
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -52,14 +52,14 @@ class departmentController extends Controller
         $contact = Departments::find($id);
         $input = $request->all();
         $contact->update($input);
-        return redirect('admin/department')->with('success', 'Departments updated successfully');  
+        return redirect('admin/department')->with('success', 'Departments updated successfully');
     }
- 
-  
+
+
     public function destroy($id)
     {
         Departments::destroy($id);
-        return redirect('admin/department')->with('success', ' Departments deleted successfully');  
+        return redirect('admin/department')->with('success', ' Departments deleted successfully');
     }
-   
+
 }
