d7a81b8459429120f2626226390ee8813aa6002c
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2022-03-28 20:49:39 +0600

    initial scale

--- resources/views/lead_generation/index.blade.php
+++ resources/views/lead_generation/index.blade.php
@@ -7,7 +7,7 @@
 	<!-- Meta
 	============================================= -->
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width,intial-scale=1,max-scale=1">
+	<meta name="viewport" content="width=device-width,intial-scale=1,maximum-scale=1">
 
 	<!-- Stylesheets
 	============================================= -->
