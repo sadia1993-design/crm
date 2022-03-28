c4ba21edaa7240e40c465b13390317a3db7a4b74
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2022-03-28 21:11:04 +0600

    item init

--- resources/views/admin/items/list_item.blade.php
+++ resources/views/admin/items/list_item.blade.php
@@ -36,7 +36,7 @@
 				<td class="highlight">{{ $item_data->name }}</td>
 				<td>{{ $item_data->description }}</td>
 				<td>{{ $item_data->rate }}</td>
-				<td>{{ $item_data->tax->rules  }}</td>
+				<td>{{ $item_data  }}</td>
 				<td>{{ $item_data->unit->unit_name }}</td>
 				<td>
 					<form action="{{ route('item.destroy',$item_data->id) }}" method="post" id="delete{{$item_data->id}}">
